0dc254c736254a5b1d81007de99d5b80f1b74a8c
Lorenzo Fornara <fw1987@gmail.com>
2024-06-01 10:01:21 +0000

    update Alpine.bat

--- Windows/Alpine.bat
+++ Windows/Alpine.bat
@@ -2,12 +2,21 @@
 set FILE="alpine.img"
 
 IF EXIST %FILE% (
-	echo file exist
+	echo file exist... -> SKIP
 	cls
 ) ELSE (
-	curl -o alpine.img -L https://github.com/h4shell/termux_alpinelinux/raw/main/alpine.img
+	curl -o %FILE% -L https://github.com/h4shell/termux_alpinelinux/raw/main/alpine.img
 )
 
-@echo off
-echo VM started: SSH: 127.0.0.1:20000
-cmd.exe /c "start /B qemu -hda alpine.img -boot c -m 1024 -netdev user,id=n1,hostfwd=tcp::20000-:22,hostfwd=tcp::20001-:20001,hostfwd=tcp::20002-:20002,hostfwd=tcp::20003-:20003,hostfwd=tcp::20004-:20004,hostfwd=tcp::20005-:20005,hostfwd=udp::20000-:20000,hostfwd=udp::20001-:20001,hostfwd=udp::20002-:20002,hostfwd=udp::20003-:20003,hostfwd=udp::20004-:20004,hostfwd=udp::20005-:20005 -device virtio-net,netdev=n1 -machine q35 -nographic -smp 8 -vga none -serial none -monitor none"
+
+echo Alpine-VM started: SSH: 127.0.0.1:20000
+echo.
+echo  ---------------------------------
+echo ^|                                 ^|
+echo ^|   SSH: 127.0.0.1: 20000         ^|
+echo ^|   Username: user                ^|
+echo ^|   Password: password            ^|
+echo ^|                                 ^|
+echo  ---------------------------------
+echo.
+cmd.exe /c "start /B qemu -hda %FILE& -boot c -m 1024 -netdev user,id=n1,hostfwd=tcp::20000-:22,hostfwd=tcp::20001-:20001,hostfwd=tcp::20002-:20002,hostfwd=tcp::20003-:20003,hostfwd=tcp::20004-:20004,hostfwd=tcp::20005-:20005,hostfwd=udp::20000-:20000,hostfwd=udp::20001-:20001,hostfwd=udp::20002-:20002,hostfwd=udp::20003-:20003,hostfwd=udp::20004-:20004,hostfwd=udp::20005-:20005 -device virtio-net,netdev=n1 -machine q35 -nographic -smp 6 -vga none -serial none -monitor none"
